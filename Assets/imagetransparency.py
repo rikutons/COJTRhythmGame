da91a56956b799a1f2855d64468ba940256f0d74
seisyun9665 <minecraft.syun0521@gmail.com>
2022-06-01 14:35:26 +0900

    集めた素材画像と、それを透過して出力するプログラムを作成

---
+++ Assets/imagetransparency.py
@@ -0,0 +1,18 @@
+# imagesの中のjpgをpng
+import os
+import cv2
+import numpy as np
+import glob
+from PIL import Image
+
+files = glob.glob("Assets/images/*.jpg")
+for tmp in files:
+    newPath = tmp[:-4] + '.png'
+
+    src = cv2.imread(tmp)
+    mask = np.all(src >= 50, axis=-1)
+    dst = cv2.cvtColor(src, cv2.COLOR_BGR2BGRA)
+    dst[mask,3] = 0
+    cv2.imwrite(newPath, dst)
+
+    os.remove(tmp)
