1ac7579cea89f23a551083406162afe8f3082042
seisyun9665 <minecraft.syun0521@gmail.com>
2022-06-22 14:45:52 +0900

    ノートの判定表示機能を追加

--- Assets/Scripts/Presenter/ScorePresenter.cs
+++ Assets/Scripts/Presenter/ScorePresenter.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -17,6 +18,15 @@ public class ScorePresenter : MonoBehaviour
     [SerializeField]
     private AudioSource audioSource;
 
+    [SerializeField]
+    private GameObject miss;
+    [SerializeField]
+    private GameObject good;
+    [SerializeField]
+    private GameObject great;
+    [SerializeField]
+    private GameObject perfect;
+
     private float defaultVolume;
 
     void Start()
@@ -34,6 +44,7 @@ public class ScorePresenter : MonoBehaviour
 
     public void AddScore(string judge)
     {
+        ShowJudgeMessage(judge);
         switch (judge)
         {
             case "perfect":
@@ -54,6 +65,40 @@ public class ScorePresenter : MonoBehaviour
             case "miss":
                 userScore.Combo = 0;
                 audioSource.volume = defaultVolume * 0.3f;
+                break;
+        }
+    }
+
+
+    private void ShowJudgeMessage(string judge) {
+        switch(judge){
+            case "perfect":
+                perfect.SetActive(false);
+                perfect.SetActive(true);
+
+                Observable.Timer(TimeSpan.FromMilliseconds(200))
+                .Subscribe(_ => perfect.SetActive(false));
+                break;
+            case "great":
+                great.SetActive(false);
+                great.SetActive(true);
+
+                Observable.Timer(TimeSpan.FromMilliseconds(200))
+                .Subscribe(_ => great.SetActive(false));
+                break;
+            case "good":
+                good.SetActive(false);
+                good.SetActive(true);
+
+                Observable.Timer(TimeSpan.FromMilliseconds(200))
+                .Subscribe(_ => good.SetActive(false));
+                break;
+            case "miss":
+                miss.SetActive(false);
+                miss.SetActive(true);
+
+                Observable.Timer(TimeSpan.FromMilliseconds(200))
+                .Subscribe(_ => miss.SetActive(false));
                 break;
         }
     }
